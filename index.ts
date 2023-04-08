9c5c7a4923919cd25fc5864af86e594a55a83136
Player Nguyên <daudaua3782@gmail.com>
2023-04-08 15:17:46 +0700

    test: add unit test

--- index.ts
+++ index.ts
@@ -1,14 +1,22 @@
 import { workspace } from "kratos-core";
-import { PathLike, existsSync } from "fs-extra";
+import { PathLike, existsSync, readJsonSync, writeJsonSync } from "fs-extra";
 import { join as pathJoin } from "path";
 
 export namespace kratosRuntime {
   export class RuntimeWorkspace extends workspace.Workspace {
     private map: RuntimeMap;
-    constructor() {
-      super("runtime");
+    constructor(cwd?: string) {
+      super(cwd !== undefined ? pathJoin(cwd, "runtime") : "runtime");
 
       this.map = new RuntimeMap(this);
+    }
+
+    /**
+     *
+     * @returns an instance of {@link RuntimeMap}
+     */
+    public getRuntimeMap() {
+      return this.map;
     }
   }
 
@@ -28,13 +36,38 @@ export namespace kratosRuntime {
       );
 
       // Check if the map is exists or not
+      let runtimeList: RuntimeMapEntry[] = [];
       if (existsSync(this._filePath)) {
-        // readJson;
+        runtimeList = readJsonSync(this._filePath, { throws: true });
+      } else {
+        // Write a new file and load it
+        writeJsonSync(this._filePath, []);
+      }
+
+      for (let r of runtimeList) {
+        const { major } = r;
+        if (this._runtimeMap.has(major)) {
+          throw new Error(`Invalid runtime map (duplicated key)`);
+        }
+
+        this._runtimeMap.set(major, r);
       }
     }
 
     public get getFilePath(): PathLike {
       return this._filePath;
+    }
+
+    public hasRuntime(major: number) {
+      return this._runtimeMap.has(major);
+    }
+
+    public getRuntime(major: number) {
+      return this._runtimeMap.get(major);
+    }
+
+    public setRuntime(runtimeEntry: RuntimeMapEntry) {
+      return this._runtimeMap.set(runtimeEntry.major, runtimeEntry);
     }
   }
 
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,1 +1,34 @@
-import { kratosRuntime } from "..";
+import { rmSync } from "fs-extra";
+import { kratosRuntime } from "./../index";
+import { expect } from "chai";
+import { existsSync } from "fs-extra";
+
+const TEST_OUTPUT = "test-output";
+function cleanupOutput() {
+  if (existsSync(TEST_OUTPUT)) {
+    // ensureDirSync(TEST_OUTPUT);
+    rmSync(TEST_OUTPUT, { force: true, recursive: true });
+  }
+}
+before(() => {
+  cleanupOutput();
+});
+
+after(() => {
+  cleanupOutput();
+});
+
+describe("[unit] assertion test", () => {
+  it(`should expect as expectation`, () => {
+    expect(true).to.be.true;
+    expect([1, 2, 3]).to.have.lengthOf.greaterThanOrEqual(3);
+  });
+});
+
+describe("[unit] RuntimeWorkspace", () => {
+  it("should be able to create an instance of RuntimeWorkspace", () => {
+    expect(true).to.be.true;
+    new kratosRuntime.RuntimeWorkspace(TEST_OUTPUT);
+    expect(() => {}).to.not.throws();
+  });
+});
