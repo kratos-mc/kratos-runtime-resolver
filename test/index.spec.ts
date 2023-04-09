e159a19a44f65b398b1e5b77b894b2c391a0dda7
Player Nguyên <daudaua3782@gmail.com>
2023-04-09 12:12:58 +0700

    test: cover RuntimeMap

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,8 +1,9 @@
-import { accessSync, rmSync } from "fs-extra";
+import { accessSync, readJsonSync, rmSync } from "fs-extra";
 import { kratosRuntime } from "./../index";
 import { expect } from "chai";
 import { existsSync } from "fs-extra";
 import fse from "fs-extra";
+import { join as pathJoin } from "path";
 
 const TEST_OUTPUT = "test-output";
 function cleanupOutput() {
@@ -36,5 +37,46 @@ describe("[unit] RuntimeWorkspace", () => {
         fse.constants.R_OK | fse.constants.W_OK | fse.constants.X_OK
       )
     ).to.not.be.throw();
+  });
+});
+
+describe("[unit] RuntimeMap", () => {
+  let runtimeWorkspace = new kratosRuntime.RuntimeWorkspace(TEST_OUTPUT);
+  it(`should make a new instance successfully`, () => {
+    let runtimeMap = new kratosRuntime.RuntimeMap(runtimeWorkspace);
+
+    expect(runtimeMap).to.not.be.undefined;
+    expect(existsSync(runtimeMap.getFilePath())).to.be.true;
+  });
+
+  it(`should success a simple operation`, () => {
+    let r = new kratosRuntime.RuntimeMap(runtimeWorkspace);
+    expect(r).not.to.be.undefined;
+    expect(r.getCacheMap()).not.to.be.undefined;
+    expect(r.getRuntime(7)).to.be.undefined;
+
+    expect(() =>
+      r.setRuntime({ major: 7, path: pathJoin(TEST_OUTPUT, "major-7") })
+    ).not.to.throws();
+    expect(r.getRuntime(7)).to.not.be.undefined;
+    expect(r.getRuntime(7)).have.keys(["major", "path"]);
+  });
+
+  it(`should save a file`, () => {
+    let r = new kratosRuntime.RuntimeMap(runtimeWorkspace);
+    r.setRuntime;
+    expect(() =>
+      r.setRuntime({ major: 7, path: pathJoin(TEST_OUTPUT, "major-7") })
+    ).not.to.throws();
+
+    r.saveFile();
+
+    let a = readJsonSync(r.getFilePath());
+    expect(a).to.not.be.undefined;
+    expect(a).to.be.an("array");
+
+    expect(a).to.have.lengthOf.gt(0);
+
+    expect(r.hasRuntime(7)).to.be.true;
   });
 });
